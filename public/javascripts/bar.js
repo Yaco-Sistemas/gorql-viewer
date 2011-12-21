b62544eb3f46eaa6f4beaf57f7515e0393a42403
Alejandro Blanco <ablanco@yaco.es>
2011-12-21 10:27:50 +0100

    Ver #24 Añado a la visualización por barras el valor escrito en cada barra

--- public/javascripts/bar.js
+++ public/javascripts/bar.js
@@ -14,6 +14,16 @@ var DV = (function () {
                 .attr("y", function (d, i) { return i * 20; })
                 .attr("width", x)
                 .attr("height", 20);
+
+            svg.selectAll("text")
+                .data(data)
+                .enter().append("svg:text")
+                .attr("x", x)
+                .attr("y", function (d, i) { return (i * 20) + 10; })
+                .attr("dx", -3) // padding-right
+                .attr("dy", ".35em") // vertical-align: middle
+                .attr("text-anchor", "end") // text-align: right
+                .text(String);
         },
 
         init = function (container, data, options) {

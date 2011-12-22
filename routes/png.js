19d1350b0cbdda556be20ab54dc8114b4d5d2514
Alejandro Blanco <ablanco@yaco.es>
2011-12-22 17:50:51 +0100

    Ver #24 Más bugfixes

--- routes/png.js
+++ routes/png.js
@@ -27,16 +27,30 @@ renderResults = function (response, params, error, results) {
 
     var convert,
         i,
+        j,
         chartData = {
             labels: [],
             values: []
         },
-        buffer = '';
+        buffer = '',
+        aux;
 
-    for (i = 0; i < results.length; i += 1) {
-        chartData.labels.push(results[i][params.chart.labels].value);
-        chartData.values.push(results[i][params.chart.values].value);
+    try {
+        for (i = 0; i < results.length; i += 1) {
+            aux = [];
+            chartData.labels.push(results[i][params.chart.labels].value);
+            for (j = 0; j < params.chart.series.length; j += 1) {
+                aux.push(parseInt(results[i][params.chart.series[j]].value, 10));
+            }
+            chartData.values.push(aux);
+        }
+        params.chart.data = chartData;
+    } catch (err) {
+        console.log(err);
+        response.send('Some error happened processing the data.', 500);
+        return;
     }
+
     convert = convertSVG(params.chart, chartData);
     response.header("Content-Type", "image/png");
 

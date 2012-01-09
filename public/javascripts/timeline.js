0ff102ac15d8a65468fc068659e0257c3ec134dd
Alejandro Blanco <ablanco@yaco.es>
2012-01-09 18:06:46 +0100

    Ver #28 Pequeñas mejoras de estilo y código

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -3,19 +3,30 @@
 
 function initTimeline() {
     "use strict";
-    var bandInfos = [
-        Timeline.createBandInfo({
-            width:          "70%",
-            intervalUnit:   Timeline.DateTime.MONTH,
-            intervalPixels: 100
-        }),
-        Timeline.createBandInfo({
-            width:          "30%",
-            intervalUnit:   Timeline.DateTime.YEAR,
-            intervalPixels: 200
-        })];
+
+    var eventSource = new Timeline.DefaultEventSource(),
+        bandInfos = [
+            Timeline.createBandInfo({
+                eventSource: eventSource,
+                width: "70%",
+                intervalUnit: Timeline.DateTime.MONTH,
+                intervalPixels: 100
+            }),
+            Timeline.createBandInfo({
+                eventSource: eventSource,
+                width: "30%",
+                intervalUnit: Timeline.DateTime.YEAR,
+                intervalPixels: 200
+            })
+        ];
 
     bandInfos[1].syncWith = 0;
     bandInfos[1].highlight = true;
     Timeline.create(document.getElementById("dv_viewport"), bandInfos);
+    eventSource.loadJSON({
+        'wiki-url': "http://www.yaco.es/", // TODO
+        'wiki-section': "Timeline",
+        'dateTimeFormat': 'Gregorian',
+        'events': []
+    });
 }

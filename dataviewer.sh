5cf779fc5f51788f94fcb28f8483f921b61e4a6a
Alejandro Blanco <ablanco@yaco.es>
2012-01-31 10:30:38 +0100

    Ver #54 El script de servicio arranca ahora el proyecto en modo 'production'

--- dataviewer.sh
+++ dataviewer.sh
@@ -2,13 +2,13 @@
 
 case $1 in
     start)
-        su dataviewer -c "/opt/dataviewer/node_modules/forever/bin/forever start /opt/dataviewer/app.js" --shell=/bin/sh
+        su dataviewer -c "NODE_ENV=production /opt/dataviewer/node_modules/forever/bin/forever start /opt/dataviewer/app.js" --shell=/bin/sh
         ;;
     stop)
         su dataviewer -c "/opt/dataviewer/node_modules/forever/bin/forever stop /opt/dataviewer/app.js" --shell=/bin/sh
         ;;
     restart)
-        su dataviewer -c "/opt/dataviewer/node_modules/forever/bin/forever restart /opt/dataviewer/app.js" --shell=/bin/sh
+        su dataviewer -c "NODE_ENV=production /opt/dataviewer/node_modules/forever/bin/forever restart /opt/dataviewer/app.js" --shell=/bin/sh
         ;;
     status)
         su dataviewer -c "/opt/dataviewer/node_modules/forever/bin/forever list" --shell=/bin/sh

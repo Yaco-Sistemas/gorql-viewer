a13b2ca6db0c508a40108408c6f75935c7cff71e
Lorenzo Gil <lgs@yaco.es>
2012-04-25 11:29:52 +0200

    Renombro dataviewer por gorql-viewer en el fichero del paquete y en el script de servicio. Ver #123

--- dataviewer.sh
+++ dataviewer.sh
@@ -2,16 +2,16 @@
 
 case $1 in
     start)
-        su dataviewer -c "NODE_ENV=production /opt/dataviewer/node_modules/forever/bin/forever start /opt/dataviewer/app.js" --shell=/bin/sh
+        su gorql -c "NODE_ENV=production /opt/gorql-viewer/node_modules/forever/bin/forever start /opt/gorql-viewer/app.js" --shell=/bin/sh
         ;;
     stop)
-        su dataviewer -c "/opt/dataviewer/node_modules/forever/bin/forever stop /opt/dataviewer/app.js" --shell=/bin/sh
+        su gorql -c "/opt/gorql-viewer/node_modules/forever/bin/forever stop /opt/gorql-viewer/app.js" --shell=/bin/sh
         ;;
     restart)
-        su dataviewer -c "NODE_ENV=production /opt/dataviewer/node_modules/forever/bin/forever restart /opt/dataviewer/app.js" --shell=/bin/sh
+        su gorql -c "NODE_ENV=production /opt/gorql-viewer/node_modules/forever/bin/forever restart /opt/gorql-viewer/app.js" --shell=/bin/sh
         ;;
     status)
-        su dataviewer -c "/opt/dataviewer/node_modules/forever/bin/forever list" --shell=/bin/sh
+        su gorql -c "/opt/gorql-viewer/node_modules/forever/bin/forever list" --shell=/bin/sh
         ;;
     *)
         echo $"Usage: $0 {start|stop|restart|status}"

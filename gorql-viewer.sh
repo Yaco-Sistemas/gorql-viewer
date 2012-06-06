b99bac01b9a35f599375f0349b9c6affb0b69362
Alejandro Blanco <ablanco@yaco.es>
2012-06-06 12:49:58 +0200

    Soporte de chkconfig

--- gorql-viewer.sh
+++ gorql-viewer.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# chkconfig: 2345 99 01
+# description: The GORQL Viewer server is a node.js application and it renders \
+#              SPARQL queries results.
+# config: /etc/gorql-viewer/settings.js
+#
+### BEGIN INIT INFO
+# Required-Start: $local_fs $network
+# Required-Stop: $local_fs $network
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Start and stop the GORQL Viewer component.
+# Description: The GORQL Viewer server is a node.js application and it renders
+#              SPARQL queries results.
+### END INIT INFO
+
 case $1 in
     start)
         su gorql -c "NODE_ENV=production forever start /opt/gorql-viewer/app.js" --shell=/bin/sh
